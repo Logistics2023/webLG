0cce233112c628616052b05a6fdbf2bd5878cae3
Your Name <you@example.com>
2024-06-12 16:04:59 -0400

    DC

--- src/app/(no-auth)/page.jsx
+++ src/app/(no-auth)/page.jsx
@@ -276,8 +276,8 @@ export default function Home() {
 
               {/* <button onClick={write}>Click</button> */}
 
-              <button type="button" onClick={() => router.push('/Calculadora')} className="w-full border-[2px]   text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-[12px] px-5 py-2.5 text-center inline-flex items-center ">
-                Contenedores
+              <button type="button" onClick={() => router.push('/Glosario')} className="w-full border-[2px]   text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-[12px] px-5 py-2.5 text-center inline-flex items-center ">
+                Glosarios
                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                 </svg>
